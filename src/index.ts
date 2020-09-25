55cb8d1c4043d4bed42ae9d776c1ff6510f03adf
Felix Geller <fgeller@gmail.com>
2020-09-25 09:21:15 +0200

    sets tray icon; updates icons/assets

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
-import {app, BrowserWindow, globalShortcut} from 'electron';
+import {
+  app,
+  BrowserWindow,
+  Menu,
+  nativeImage,
+  Tray,
+  globalShortcut,
+} from 'electron';
 declare const MAIN_WINDOW_WEBPACK_ENTRY: any;
+const path = require('path');
 
 if (require('electron-squirrel-startup')) {
   app.quit();
 }
 
+let tray: Tray = null;
+
 const createWindow = () => {
+  const assetsPath = app.isPackaged
+    ? path.join(process.resourcesPath, 'assets')
+    : 'assets';
+
+  const imgPath = path.join(process.resourcesPath, 'tray.png');
+  console.log('imgPath', imgPath);
+  let image = nativeImage.createFromPath(imgPath);
+
+  tray = new Tray(image);
+  tray.setToolTip('catalyst');
+  tray.setContextMenu(Menu.buildFromTemplate([]));
+
+  tray.on('click', function (ev) {
+    console.log('click tray', ev);
+    activate();
+  });
+
+  console.log('tray', tray);
+
   const winHeight = 58;
   const winWidth = 400;
-
   const mainWindow = new BrowserWindow({
     height: winHeight,
     width: winWidth,
@@ -38,7 +66,7 @@ function ready(): void {
   globalShortcut.register('CommandOrControl+Space', activate);
 
   if (app.dock) {
-    app.dock.hide();
+    //app.dock.hide();
   }
   createWindow();
 }
